02d2052035964dbc9e0ab64031ba2e0768d7751d
spamalope01 <spamalope01@yahoo.com>
2018-10-24 09:14:06 -0700

    first classes of react

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+class StorePicker extends React.Component {
+  render() {
+    return <form className="store-selector"></form>
+    return form.store-selecto 
+
+
+  }
+}
+
+
+export default StorePicker;
