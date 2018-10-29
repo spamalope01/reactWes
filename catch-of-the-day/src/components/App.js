7821ca365407ac5517d9ffaed6f897b15787b62c
spamalope01 <spamalope01@yahoo.com>
2018-10-29 13:45:02 -0700

    just learned state

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -1,19 +1,30 @@
-import React from 'react';
-import Header from './Header';
-import Order from './Order';
-import Inventory from './Inventory';
+import React from "react";
+import Header from "./Header";
+import Order from "./Order";
+import Inventory from "./Inventory";
 
 
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+  addFish = (fish) => {
+    const fishes = {...this.state.fishes};
+    fishes[`fish${Date.now()}`] = fish;
+    this.setState({
+      fishes: fishes
+    })
+  }
   render() {
     return (
-      <div className="catch-of-the-Day">
+      <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market"/>
         </div>
       <Order />
-      <Inventory />
+      <Inventory addFish={this.addFish}/>
     </div>
   );
   }
